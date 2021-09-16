4c9c8dd6fce1b6874002c3577ce9a069dc08e63f
0xBlockchain <33234035+0xBlockchain@users.noreply.github.com>
2021-09-15 21:47:55 -0400

    revert useMemo hook update

--- front/src/theme/index.js
+++ front/src/theme/index.js
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import { useMemo } from 'react';
 import { CssBaseline } from '@material-ui/core';
 import { createTheme, ThemeProvider, StyledEngineProvider } from '@material-ui/core/styles';
 import useSettings from '../hooks/useSettings';
@@ -14,7 +14,7 @@ export default function ThemeConfig({ children }) {
   const { themeMode, themeDirection } = useSettings();
   const isLight = themeMode === 'light';
 
-  const themeOptions = React.useMemo(
+  const themeOptions = useMemo(
     () => ({
       palette: isLight ? { ...palette.light, mode: 'light' } : { ...palette.dark, mode: 'dark' },
       typography,
