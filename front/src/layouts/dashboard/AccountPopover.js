0d1cd54f416ac066e471e877ae566c87cd82b564
Yamada <cool.developer24@gmail.com>
2021-10-06 21:24:38 -0400

    add profile page

--- front/src/layouts/dashboard/AccountPopover.js
+++ front/src/layouts/dashboard/AccountPopover.js
@@ -1,18 +1,19 @@
-import { useState, useEffect, useCallback } from 'react'
-import { Avatar, Stack, Typography } from '@material-ui/core'
-import SvgIconStyle from 'components/SvgIconStyle'
-import * as React from 'react';
-import Popover from '@mui/material/Popover';
-import Divider from '@mui/material/Divider';
-import Button from '@mui/material/Button';
-import "./customPopover.css"
+import { useState, useEffect, useCallback } from "react";
+import { Link as RouterLink } from "react-router-dom";
+import { Avatar, Stack, Typography } from "@material-ui/core";
+import SvgIconStyle from "components/SvgIconStyle";
+import * as React from "react";
+import Popover from "@mui/material/Popover";
+import Divider from "@mui/material/Divider";
+import Button from "@mui/material/Button";
+import "./customPopover.css";
 export default function AccountPopover() {
   const [userName, setUserName] = useState("");
   const [email, setEmail] = useState("");
 
   useEffect(() => {
     handleDashboard();
-  })
+  });
 
   const [anchorEl, setAnchorEl] = React.useState(null);
   const handleClick = (event) => {
@@ -22,34 +23,30 @@ export default function AccountPopover() {
     setAnchorEl(null);
   };
   const open = Boolean(anchorEl);
-  const id = open ? 'simple-popover' : undefined;
+  const id = open ? "simple-popover" : undefined;
 
-  const handleDashboard = useCallback(
-    async () => {
-      try {
-        if (localStorage.getItem("username") && localStorage.getItem("email")) {
-          setUserName(localStorage.getItem("username"))
-          setEmail(localStorage.getItem("email"))
-        }
-      } catch (err) {
-        console.log("Error for email verification", err);
+  const handleDashboard = useCallback(async () => {
+    try {
+      if (localStorage.getItem("username") && localStorage.getItem("email")) {
+        setUserName(localStorage.getItem("username"));
+        setEmail(localStorage.getItem("email"));
       }
-    },
-    []
-  )
+    } catch (err) {
+      console.log("Error for email verification", err);
+    }
+  }, []);
 
   return (
     <>
       <Stack
         direction="row"
         sx={{
-
           height: 54,
           width: { xs: 170, md: 270 },
-          border: '2px solid rgba(238, 238, 238, 0.5)',
-          background: 'linear-gradient(180deg, #7C1EFB 0%, #AF56BB 100%)',
-          boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-          borderRadius: '100px',
+          border: "2px solid rgba(238, 238, 238, 0.5)",
+          background: "linear-gradient(180deg, #7C1EFB 0%, #AF56BB 100%)",
+          boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+          borderRadius: "100px",
           pl: 1,
           pr: 2,
         }}
@@ -58,15 +55,17 @@ export default function AccountPopover() {
         justifyContent="space-between"
       >
         <Avatar src="/images/avatar.png" alt="avatar" />
-        <Stack sx={{ color: 'black', ml: 2, mr: 2 }}>
-          <Typography color="white" sx={{ fontSize: {md:18,xs:14} }}>{userName}Lois James</Typography>
-          <Typography color="white" sx={{ fontSize: {md:12,xs:10} }}>
+        <Stack sx={{ color: "black", ml: 2, mr: 2 }}>
+          <Typography color="white" sx={{ fontSize: { md: 18, xs: 14 } }}>
+            {userName}Lois James
+          </Typography>
+          <Typography color="white" sx={{ fontSize: { md: 12, xs: 10 } }}>
             {email}David@email.com
           </Typography>
         </Stack>
         <SvgIconStyle
           src="/icons/arrow-down.svg"
-          sx={{ width: '16px', height: '8px', backgroundColor: 'black' }}
+          sx={{ width: "16px", height: "8px", backgroundColor: "black" }}
         />
       </Stack>
       <Popover
@@ -75,47 +74,57 @@ export default function AccountPopover() {
         anchorEl={anchorEl}
         onClose={handleClose}
         anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
+          vertical: "top",
+          horizontal: "left",
         }}
         transformOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
+          vertical: "top",
+          horizontal: "left",
         }}
         sx={{
           top: { xs: -11, md: -3 },
-
         }}
-
       >
         <Stack
           sx={{
             height: 210,
             width: { md: 270, xs: 170 },
-            border: '2px solid rgba(238, 238, 238, 0.5)',
-            background: 'linear-gradient(180deg, #7C1EFB 0%, #AF56BB 100%)',
-            boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-            borderRadius: '30px',
+            border: "2px solid rgba(238, 238, 238, 0.5)",
+            background: "linear-gradient(180deg, #7C1EFB 0%, #AF56BB 100%)",
+            boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+            borderRadius: "30px",
             pl: 1,
             pr: 1,
           }}
-
         >
           <Stack alignItems="center">
-            <Avatar sx={{ mt: 2, }} src="/images/avatar.png" alt="avatar" />
-            <Typography color="white" sx={{ fontSize: 18 }}>Lois James</Typography>
+            <Avatar sx={{ mt: 2 }} src="/images/avatar.png" alt="avatar" />
+            <Typography color="white" sx={{ fontSize: 18 }}>
+              Lois James
+            </Typography>
             <Typography color="white" sx={{ fontSize: 12 }}>
               David@email.com
             </Typography>
             <Divider light color="white" sx={{ width: 200 }} />
           </Stack>
-          <Typography color="white" sx={{ fontSize: 18, ml: 3 }}>My account</Typography>
-          <Typography color="white" sx={{ fontSize: 18, ml: 3 }}>Settings</Typography>
+          <Typography
+            component={RouterLink}
+            to="/dashboard/profile"
+            color="white"
+            sx={{ fontSize: 18, ml: 3, cursor: "pointer" }}
+          >
+            My account
+          </Typography>
+          <Typography color="white" sx={{ fontSize: 18, ml: 3 }}>
+            Settings
+          </Typography>
           <Stack alignItems="center" sx={{ mt: 1 }}>
-            <Button variant="contained" sx={{ height: 30 }}>sign out</Button>
+            <Button variant="contained" sx={{ height: 30 }}>
+              sign out
+            </Button>
           </Stack>
         </Stack>
       </Popover>
     </>
-  )
+  );
 }
---
+++ front/src/pages/UserProfile.js
@@ -0,0 +1,272 @@
+import { merge } from "lodash";
+import { useSnackbar } from "notistack5";
+import {
+  Container,
+  Typography,
+  Box,
+  Stack,
+  Button,
+  Grid,
+  Divider,
+  Table,
+  LinearProgress,
+  TextField,
+  MenuItem,
+  Switch,
+  FormControlLabel,
+} from "@material-ui/core";
+import { styled } from "@material-ui/core/styles";
+import { useState, useEffect, useCallback } from "react";
+import axios from "utils/axios";
+import * as Yup from "yup";
+import { useFormik, Form, FormikProvider } from "formik";
+import UploadSingleFile from "components/UploadSingleFile";
+
+const CardStyle = styled(Box)(({ theme }) => ({
+  background:
+    "linear-gradient(180deg, rgba(248, 255, 255, 0.15) 0%, rgba(156, 255, 249, 0.15) 100%)",
+  border: "5px solid #964CFA",
+  boxSizing: "border-box",
+  borderRadius: "16px",
+  padding: theme.spacing(4),
+  width: "50%",
+  margin: "auto",
+}));
+
+export default function Dashboard() {
+  const [user, setUser] = useState();
+  const token = localStorage.getItem("token");
+  useEffect(() => {
+    async function fetch() {
+      const useremail = localStorage.getItem("email");
+
+      const url =
+        process.env.REACT_APP_BASE_URL +
+        `/api/users/profile/get?email=${useremail}`;
+      console.log("server url: ", url);
+      const result = await axios.get(url, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      setUser(result.data);
+    }
+    fetch();
+  }, []);
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+  const ProfileSchema = Yup.object().shape({
+    first_name: Yup.string().required("First Name is required"),
+    last_name: Yup.string().required("Last Name is required"),
+    user_name: Yup.string().required("User Name is required"),
+    password: Yup.string().required("Password is required"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      first_name: user?.first_name,
+      last_name: user?.last_name,
+      user_name: user?.user_name,
+      password: user?.password,
+      telegram_id: "",
+      twitter_id: "",
+      email: user?.email,
+      wallet_address: "",
+      KYC_Completed: "level1",
+      MFA: false,
+      cover: "",
+    },
+    validationSchema: ProfileSchema,
+    onSubmit: async (values, { setErrors, setSubmitting, resetForm }) => {
+      try {
+        const {
+          first_name,
+          last_name,
+          user_name,
+          password,
+          cover,
+          email,
+          wallet_address,
+          telegram_id,
+          twitter_id,
+        } = values;
+        console.log(values);
+        // const formData = new FormData();
+        // formData.append("image", cover);
+        // formData.append("name", name);
+        // formData.append("description", description);
+        const url =
+          process.env.REACT_APP_BASE_URL + `/api/users/profile/update`;
+        console.log("server url: ", url);
+        const result = await axios.post(
+          url,
+          {
+            first_name,
+            last_name,
+            user_name,
+            wallet_address,
+            telegram_id,
+            twitter_id,
+          },
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
+        console.log(values);
+        enqueueSnackbar("User updated successfully", { variant: "success" });
+        resetForm();
+      } catch (error) {
+        console.error(error);
+        enqueueSnackbar("Oops! An error occured", { variant: "success" });
+        setSubmitting(false);
+      }
+    },
+  });
+
+  const {
+    values,
+    errors,
+    touched,
+    handleSubmit,
+    isSubmitting,
+    getFieldProps,
+    setFieldValue,
+  } = formik;
+
+  const handleDrop = useCallback(
+    (acceptedFiles) => {
+      const file = acceptedFiles[0];
+      if (file) {
+        setFieldValue("cover", file);
+      }
+    },
+    [setFieldValue]
+  );
+
+  const levels = ["level1", "level2", "level3"];
+
+  return (
+    <Container maxWidth="xl">
+      <CardStyle>
+        <FormikProvider value={formik}>
+          <form onSubmit={handleSubmit}>
+            <Stack direction="row" spacing={5} alignItems="flex-start">
+              <Box
+                sx={{
+                  width: 200,
+                  height: 200,
+                  borderRadius: 1,
+                }}
+              >
+                <UploadSingleFile
+                  maxSize={31457280}
+                  accept="image/*"
+                  file={
+                    values.cover && {
+                      preview: URL.createObjectURL(values.cover),
+                    }
+                  }
+                  onDrop={(e) => handleDrop(e, 1)}
+                  error={Boolean(touched.cover && errors.cover)}
+                  sx={{ height: 200 }}
+                />
+              </Box>
+              <Stack spacing={3}>
+                <Stack
+                  direction="row"
+                  justifyContent="space-between"
+                  flexGrow="1"
+                  spacing={3}
+                >
+                  <TextField
+                    fullWidth
+                    label="First Name"
+                    {...getFieldProps("first_name")}
+                    error={Boolean(touched.first_name && errors.first_name)}
+                    helperText={touched.first_name && errors.first_name}
+                  />
+                  <TextField
+                    fullWidth
+                    label="Last Name"
+                    {...getFieldProps("last_name")}
+                    error={Boolean(touched.last_name && errors.last_name)}
+                    helperText={touched.last_name && errors.last_name}
+                  />
+                </Stack>
+                <TextField
+                  fullWidth
+                  label="User Name"
+                  {...getFieldProps("user_name")}
+                  error={Boolean(touched.user_name && errors.user_name)}
+                  helperText={touched.user_name && errors.user_name}
+                />
+                <TextField
+                  fullWidth
+                  label="Email"
+                  {...getFieldProps("email")}
+                  error={Boolean(touched.email && errors.email)}
+                  helperText={touched.email && errors.email}
+                  disabled
+                />
+                <TextField
+                  fullWidth
+                  label="Password"
+                  {...getFieldProps("password")}
+                  error={Boolean(touched.password && errors.password)}
+                  helperText={touched.password && errors.password}
+                />
+                <TextField
+                  fullWidth
+                  label="Telegram"
+                  {...getFieldProps("telegram_id")}
+                  error={Boolean(touched.telegram_id && errors.telegram_id)}
+                  helperText={touched.telegram_id && errors.telegram_id}
+                />
+                <TextField
+                  fullWidth
+                  label="Twitter"
+                  {...getFieldProps("twitter_id")}
+                  error={Boolean(touched.twitter_id && errors.twitter_id)}
+                  helperText={touched.twitter_id && errors.twitter_id}
+                />
+                <TextField
+                  fullWidth
+                  label="Wallet Address"
+                  {...getFieldProps("wallet_address")}
+                  error={Boolean(
+                    touched.wallet_address && errors.wallet_address
+                  )}
+                  helperText={touched.wallet_address && errors.wallet_address}
+                />
+                <Stack direction="row" flexGrow="1" spacing={5}>
+                  <FormControlLabel
+                    value="start"
+                    control={
+                      <Switch color="primary" {...getFieldProps("MFA")} />
+                    }
+                    label="MFA"
+                    labelPlacement="start"
+                  />
+                  <TextField
+                    id="outlined-select-currency"
+                    select
+                    label="KYC Completed"
+                    value={levels}
+                    sx={{ flexGrow: 1 }}
+                    {...getFieldProps("KYC_Completed")}
+                    // onChange={handleChange}
+                  >
+                    {levels.map((option) => (
+                      <MenuItem key={option} value={option}>
+                        {option}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+                </Stack>
+                <Button variant="contained" type="submit">Edit</Button>
+              </Stack>
+            </Stack>
+          </form>
+        </FormikProvider>
+      </CardStyle>
+    </Container>
+  );
+}
