619b828c24ed9aef229af5ee4d4b00ef9444cc3c
0xBlockchain <buckoscarengr@gmail.com>
2021-11-09 13:43:26 -0500

    fix temp migration file

--- back/app/migrations/20211109184135-add-temp-field-users.js
+++ back/app/migrations/20211109184135-add-temp-field-users.js
@@ -18,6 +18,6 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
-     await queryInterface.removeColumn('users', 'email' );
+     await queryInterface.removeColumn('users', 'temp' );
   }
 };
