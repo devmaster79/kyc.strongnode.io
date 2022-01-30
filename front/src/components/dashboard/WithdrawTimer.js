bbda3f7b22b260a0f7a041620cb694cfe90de691
Yamada <cool.developer24@gmail.com>
2022-01-29 21:22:58 -0500

    update logic

---
+++ front/src/components/dashboard/WithdrawTimer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useTimer } from "react-timer-hook";
+
+export default function WithdrawTimer({ expiryTimestamp, setWithdrawable }) {
+  const { seconds, minutes, hours, days } = useTimer({
+    expiryTimestamp,
+    onExpire: () => setWithdrawable(true),
+  });
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <div style={{ fontSize: "20px" }}>
+        <span>
+          {days}Days {hours}Hrs {minutes}Mins {seconds}Secs
+        </span>
+      </div>
+    </div>
+  );
+}
