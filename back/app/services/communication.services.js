fa6d5f427545a33bfeb0e92545bd7805b8c0aedc
Matthew H <matej.husak@icloud.com>
2022-02-08 16:50:02 +0100

    updated sendTemplatedEmail in service

--- back/app/services/communication.services.js
+++ back/app/services/communication.services.js
@@ -1,3 +1,4 @@
+const dotenv = require("dotenv")
 dotenv.config()
 
 // regExp used for checking inputs
@@ -8,7 +9,18 @@ const AWS = require('aws-sdk');
 const defaultEmailSource = "Notifications <no-reply@strongnode.io>"
 
 /**
+ * Object that holds template names from jsons/email-template.json.
+ * Just to makes things easier. todo think about getting values directly from json above
+ * @type {{resetPassword: string, confirmEmail: string}}
+ */
+exports.emailTemplatesNames = {
+    confirmEmail: 'EmailTemplate',
+    resetPassword: 'ResetPasswordTemplate'
+}
+
+/**
  * Method that is used for sending !ONE! templated email.
+ * By default, this method uses emailTemplate for email verification.
  * @param options
  * @returns {Promise<boolean>}
  */
@@ -24,16 +36,19 @@ exports.sendTemplatedEmail = async (to, templateData = '{ "link":"unknown"}', te
     // default AWS SES email options
     let defaultEmailOptions = {
         Destination: {
-            ToAddresses: to
+            ToAddresses: [to]
         },
         TemplateData: templateData,
         Source: defaultEmailSource,
         Template: templateName
     }
 
-    // if being called from localhost, change the endpoint to localstack
-    if (process.env.AWS_LOCALSTACK_URL != '')
+    // if being called from localhost, change the endpoint to localstack, also show template data since the localstack
+    // does not send emails
+    if (process.env.AWS_LOCALSTACK_URL != '') {
         sesOptions.endpoint = process.env.AWS_LOCALSTACK_URL
+        console.log(templateData)
+    }
 
     const ses = new AWS.SES(sesOptions)
     return await ses.sendTemplatedEmail(defaultEmailOptions).promise()
