f5cd3b7c76887a9d98f998c88c6532028141488d
Matthew H <matej.husak@icloud.com>
2022-02-17 16:50:31 +0100

    added auto migrations

--- back/app/models/index.js
+++ back/app/models/index.js
@@ -1,6 +1,7 @@
-const dbConfig = require("../config/db.config.js");
+const dbConfig = require('../config/db.config.js');
+const { Umzug, SequelizeStorage } = require('umzug');
 
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
@@ -14,14 +15,28 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   }
 });
 
+// setup migrations
+const umzug = new Umzug({
+  migrations: { glob: __dirname + '/../migrations/*.js' },
+  context: sequelize.getQueryInterface(),
+  storage: new SequelizeStorage({ sequelize }),
+  logger: console
+});
+
+(async () => {
+  console.log('Starting DB migrations')
+  await umzug.up();
+  console.log('Migrations finished')
+})();
+
 const db = {};
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.history = require("./history.model.js")(sequelize, Sequelize);
-db.news = require("./news.model.js")(sequelize, Sequelize);
-db.users = require("./user.model.js")(sequelize, Sequelize);
-db.passwordreset = require('./passwordreset.model.js')(sequelize, Sequelize)
+db.history = require('./history.model.js')(sequelize, Sequelize);
+db.news = require('./news.model.js')(sequelize, Sequelize);
+db.users = require('./user.model.js')(sequelize, Sequelize);
+db.passwordreset = require('./passwordreset.model.js')(sequelize, Sequelize);
 
 module.exports = db;
