5eb4d39ac44b9e43b5d23d010cf901704362e19c
Matthew H <matej.husak@icloud.com>
2022-02-17 16:50:15 +0100

    refactored actuall migration files

--- back/app/migrations/20220203132126-passwordReset.js
+++ back/app/migrations/20220203132126-passwordReset.js
@@ -1,43 +1,31 @@
-'use strict';
+const { Sequelize } = require('sequelize');
 
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-    await queryInterface.createTable('passwordReset', {
-      id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-        allowNull: false
-      },
-      userEmail: {
-        type: Sequelize.STRING
-      },
-      token: {
-        type: Sequelize.STRING
-      },
-      status: {
-        type: Sequelize.DataTypes.ENUM('active', 'inactive')
-      },
-      createdAt: {
-        type: Sequelize.DATE,
-        allowNull: false
-      }
-    })
-  },
+async function up({ context: queryInterface }) {
+  await queryInterface.createTable('passwordReset', {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false
+    },
+    userEmail: {
+      type: Sequelize.STRING
+    },
+    token: {
+      type: Sequelize.STRING
+    },
+    status: {
+      type: Sequelize.DataTypes.ENUM('active', 'inactive')
+    },
+    createdAt: {
+      type: Sequelize.DATE,
+      allowNull: false
+    }
+  })
+}
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-    await queryInterface.dropTable('passwordReset')
-  }
-};
+async function down({ context: queryInterface }) {
+  await queryInterface.dropTable('passwordReset')
+}
+
+module.exports = { up, down };
