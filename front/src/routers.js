57836882ac0c8ecb3d8d0ec186591c1e492f9dd7
Yamada <cool.developer24@gmail.com>
2022-02-18 01:30:41 -0500

    ticket 135

--- front/src/routers.js
+++ front/src/routers.js
@@ -16,45 +16,73 @@ import CreateAccountPassword from './pages/createaccountpassword';
 import PrivateSaleInterestForm from './pages/privatesaleinterestform';
 import KYC from './pages/kyc';
 import AddData from './pages/AddData';
-import ForgottenPassword from "./pages/forgottenPassword";
-import CreateNewPassword from "./pages/createNewPassword";
+import ForgottenPassword from './pages/forgottenPassword';
+import CreateNewPassword from './pages/createNewPassword';
 
 export default function Router() {
-  return useRoutes([
-    {
-      path: 'dashboard',
-      element: <DashboardLayout />,
-      children: [
-        { path: '/dashboard', element: <Navigate to="/dashboard/app" replace /> },
-        { path: 'app', element: <Dashboard /> },
-        { path: 'profile', element: <UserProfile /> },
-        { path: 'growth', element: <Dashboard /> },
-        { path: 'sync', element: <Dashboard /> },
-        { path: 'shield', element: <Dashboard /> },
-        { path: 'add_data', element: <AddData /> }
-      ]
-    },
-    {
-      path: '*',
-      children: [
-        { path: '/*', element: <Navigate to="/signup" replace /> },
-        { path: 'signup', element: <Signup /> },
-        { path: 'signin', element: <Signin /> },
-        { path: 'magiclink', element: <MagicLink /> },
-        { path: 'signinpass', element: <SigninPass /> },
-        { path: 'signintwostep', element: <SigninTwoStep /> },
-        { path: 'signinsms', element: <SigninSMS /> },
-        { path: 'profile', element: <Profile /> },
-        { path: "forgotten-password", element: <ForgottenPassword /> },
-        { path: "create-new-password", element: <CreateNewPassword /> },
-        { path: 'sent-email', element: <SentEmail /> },
-        { path: 'verifyEmail', element: <CreateAccountPassword /> },
-        {
-          path: 'private-sale-interest-form',
-          element: <PrivateSaleInterestForm />
-        },
-        { path: 'kyc', element: <KYC /> }
-      ]
-    }
-  ]);
+  if (localStorage.getItem('username') && localStorage.getItem('email')) {
+    return useRoutes([
+      {
+        path: 'dashboard',
+        element: <DashboardLayout />,
+        children: [
+          { path: '/dashboard', element: <Navigate to="/dashboard/app" replace /> },
+          { path: 'app', element: <Dashboard /> },
+          { path: 'profile', element: <UserProfile /> },
+          { path: 'growth', element: <Dashboard /> },
+          { path: 'sync', element: <Dashboard /> },
+          { path: 'shield', element: <Dashboard /> },
+          { path: 'add_data', element: <AddData /> }
+        ]
+      },
+      {
+        path: '*',
+        element : <Signup />,
+        children: [
+          { path: '/*', element: <Navigate to="/signup" replace /> },
+          { path: 'signup', element: <Signup /> },
+          { path: 'signin', element: <Signin /> },
+          { path: 'magiclink', element: <MagicLink /> },
+          { path: 'signinpass', element: <SigninPass /> },
+          { path: 'signintwostep', element: <SigninTwoStep /> },
+          { path: 'signinsms', element: <SigninSMS /> },
+          { path: 'profile', element: <Profile /> },
+          { path: 'forgotten-password', element: <ForgottenPassword /> },
+          { path: 'create-new-password', element: <CreateNewPassword /> },
+          { path: 'sent-email', element: <SentEmail /> },
+          { path: 'verifyEmail', element: <CreateAccountPassword /> },
+          {
+            path: 'private-sale-interest-form',
+            element: <PrivateSaleInterestForm />
+          },
+          { path: 'kyc', element: <KYC /> }
+        ]
+      }
+    ]);
+  } else {
+    return useRoutes([
+      {
+        path: '*',
+        children: [
+          { path: '/*', element: <Navigate to="/signup" replace /> },
+          { path: 'signin', element: <Signin /> },
+          { path: 'signup', element: <Signup /> },
+          { path: 'magiclink', element: <MagicLink /> },
+          { path: 'signinpass', element: <SigninPass /> },
+          { path: 'signintwostep', element: <SigninTwoStep /> },
+          { path: 'signinsms', element: <SigninSMS /> },
+          { path: 'profile', element: <Profile /> },
+          { path: 'forgotten-password', element: <ForgottenPassword /> },
+          { path: 'create-new-password', element: <CreateNewPassword /> },
+          { path: 'sent-email', element: <SentEmail /> },
+          { path: 'verifyEmail', element: <CreateAccountPassword /> },
+          {
+            path: 'private-sale-interest-form',
+            element: <PrivateSaleInterestForm />
+          },
+          { path: 'kyc', element: <KYC /> }
+        ]
+      }
+    ]);
+  }
 }
