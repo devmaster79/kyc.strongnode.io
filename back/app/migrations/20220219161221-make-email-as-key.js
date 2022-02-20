4559277901539590d6ef33286fca62411503dbf8
Matthew H <matej.husak@icloud.com>
2022-02-20 19:48:18 +0100

    updated email as a unique key

---
+++ back/app/migrations/20220219161221-make-email-as-key.js
@@ -0,0 +1,19 @@
+const { Sequelize } = require('sequelize');
+
+async function up({ context: queryInterface }) {
+  await queryInterface.changeColumn('users', 'email', {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  })
+}
+
+async function down({ context: queryInterface }) {
+  await queryInterface.changeColumn('users', 'email', {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  })
+}
+
+module.exports = { up, down };
