133898db4fb0767786b6cda059b15ae71cb03a8e
David Biró <dbiro97@gmail.com>
2022-02-22 17:37:57 +0100

    Make sendSMSLimit resolveable

--- back/app/middleware/limits.js
+++ back/app/middleware/limits.js
@@ -3,15 +3,12 @@ const { createLimit, identifyByAuth } = require("./utils/createLimit");
 /**
  * Limit to prevent SMS flood attack.
  * Characteristics (T= trial): T T 5 T 5 T 5 T 5 ...
- * This shouldn't be resolved, that's why it does not expose the resolver.
  */
-exports.sendSMSLimit = {
-    limiter: createLimit("smsSend", identifyByAuth, {
-        maxFreeTrials: 2, // After 2 trials and every consecutive trials
-        banMinutesBase: 5, // 5 min ban
-        multiplier: 1, // Do not increase the time
-    }).limiter
-}
+exports.sendSMSLimit = createLimit("smsSend", identifyByAuth, {
+    maxFreeTrials: 2, // After 2 trials and every consecutive trials
+    banMinutesBase: 5, // 5 min ban
+    multiplier: 1, // Do not increase the time
+});
 
 /**
  * Limit to prevent brute force OTP attack.
