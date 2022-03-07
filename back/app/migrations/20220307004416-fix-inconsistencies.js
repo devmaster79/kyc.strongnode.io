7c7b4270144fb2739dff6f17cb97a1faa3e7a5cf
David Biró <dbiro97@gmail.com>
2022-03-07 06:42:37 +0100

    Add migration that fixes inconsistencies between db and definition

---
+++ back/app/migrations/20220307004416-fix-inconsistencies.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize');
+
+async function up({ context: queryInterface }) {
+  await queryInterface.addColumn('users', 'updatedAt', Sequelize.DATE);
+  await queryInterface.addColumn('histories', 'updatedAt', Sequelize.DATE);
+  await queryInterface.addColumn('news', 'updatedAt', Sequelize.DATE);
+}
+
+async function down({ context: queryInterface }) {
+  await queryInterface.removeColumn('users', 'updatedAt' );
+  await queryInterface.removeColumn('histories', 'updatedAt');
+  await queryInterface.removeColumn('news', 'updatedAt');
+}
+
+module.exports = { up, down };
