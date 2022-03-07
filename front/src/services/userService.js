e5c95befc358d08839a9e563752e45800f954c98
David Biró <dbiro97@gmail.com>
2022-03-07 07:05:39 +0100

    Remove verifyEmail from userServices

--- front/src/services/userService.js
+++ front/src/services/userService.js
@@ -1,17 +1,9 @@
 import axios from 'axios';
-import {
-  upload_profile_img,
-  verify_email_url,
-  profile_url,
-  investor_url
-} from '../utils/config';
+import { upload_profile_img, profile_url, investor_url } from '../utils/config';
 
 export default {
   getProfile() {
     return axios.get(profile_url);
-  },
-  verifyEmail(data) {
-    return axios.put(verify_email_url, data);
   },
   createProfile(data) {
     return axios.post(profile_url, data);
@@ -23,11 +15,10 @@ export default {
     return axios.put(profile_url, data);
   },
   uploadProfileImage(email, user_name, img_data) {
-    return axios.put(upload_profile_img,
-      {
-        email: email,
-        user_name: user_name,
-        image_data: img_data
-      });
+    return axios.put(upload_profile_img, {
+      email: email,
+      user_name: user_name,
+      image_data: img_data
+    });
   }
-}
+};
