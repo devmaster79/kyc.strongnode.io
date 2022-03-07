75584ae5a6717ae6159b73560c7680796843c43f
David Biró <dbiro97@gmail.com>
2022-03-07 06:38:54 +0100

    Add shared folder

---
+++ shared/shared-routes.js
@@ -0,0 +1,7 @@
+// keep them without leading '/'
+exports.VERIFY_EMAIL = 'verify-email';
+exports.REGISTER = 'register';
+exports.SIGN_IN_WITH_PASSWORD = 'sign-in-with-password';
+exports.SIGN_IN_WITH_AUTHENTICATOR = 'sign-in-with-authenticator';
+exports.SIGN_IN_WITH_SMS = 'sign-in-with-sms';
+exports.SIGN_IN_WITH_TOKEN = 'sign-in-with-token';
