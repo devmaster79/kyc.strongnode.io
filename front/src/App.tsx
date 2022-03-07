78ec4fe0a350ba70c47173bf3d4875498a223fa9
David Biró <dbiro97@gmail.com>
2022-03-07 06:59:19 +0100

    Add token setter from query parameters

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,20 @@
 import React, { Suspense } from 'react';
 import Routes from './routers';
 import { SnackbarProvider } from 'notistack5';
+import { useSearchParams } from 'react-router-dom';
+import * as authService from 'services/auth';
 
 const ThemeConfig = React.lazy(() => import('./theme'));
 
 function App() {
+  // Setup token given in the URL.
+  // Used by authentication.
+  const [searchParams] = useSearchParams();
+  const token = searchParams.get('token');
+  if (token) {
+    authService.setToken(token);
+  }
+
   return (
     <Suspense fallback={<div></div>}>
       <ThemeConfig>
