d04e42d58debabcb42e9502f7190b14419b3fd1c
David Biró <dbiro97@gmail.com>
2022-03-07 06:56:21 +0100

    Remove unused schemas

--- front/src/static/formSchemas.js
+++ front/src/static/formSchemas.js
@@ -3,30 +3,10 @@ import * as yup from 'yup';
 export const signupSchema = yup.object({
   first_name: yup.string().required('Please enter first name.'),
   last_name: yup.string().required('Please enter last name.'),
-  email: yup.string().email().required('Please enter email.'),
   user_name: yup.string().required('Please enter user name.'),
   termsAgreement: yup.bool().oneOf([true], 'You should agree terms and condition for sign up.')
 });
 
-export const singinSchema = yup.object({
+export const verifyEmailSchema = yup.object({
   email: yup.string().email().required('Please enter email.')
-});
-
-export const forgottenPassword = yup.object({
-  email: yup.string().email().required('Please enter email.')
-})
-
-export const createNewPassword = yup.object({
-})
-
-export const changePasswordSchema = yup.object().shape({
-  old_password: yup.string().required('Old password is required'),
-  new_password: yup.string().required('New password is required'),
-  confirm_password: yup.string().when("new_password", {
-    is: val => (val && val.length > 0 ? true : false),
-    then: yup.string().oneOf(
-      [yup.ref("new_password")],
-      "Both password need to be the same"
-    )
-  }),
 });
