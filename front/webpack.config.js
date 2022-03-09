01b24f7e35596e479e215ab85bbca33aaec74ca1
Matthew H <matej.husak@icloud.com>
2022-03-09 16:52:38 +0100

    added css loader

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -22,6 +22,10 @@ module.exports = {
             {
                 test   : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
                 loader : 'file-loader'
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
             }
         ]
     },
