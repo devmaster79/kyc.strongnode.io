de69ea377a3e47c6a39319de639e1a85053f7ea7
Matthew H <matej.husak@icloud.com>
2022-03-23 21:33:55 +0100

    edited top padding for pages

--- front/src/layouts/dashboard/index.js
+++ front/src/layouts/dashboard/index.js
@@ -22,11 +22,11 @@ const MainStyle = styled('div')(({ theme }) => ({
   flexGrow: 1,
   overflow: 'auto',
   minHeight: '100%',
-  paddingTop: APP_BAR_MOBILE + 24,
+  paddingTop: 32,
   paddingLeft: theme.spacing(2),
   paddingRight: theme.spacing(2),
   [theme.breakpoints.up('lg')]: {
-    paddingTop: APP_BAR_DESKTOP + 24
+    paddingTop: 32
   }
 }));
 
