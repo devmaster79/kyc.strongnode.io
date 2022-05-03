2a934e124ee46f9b17dea5aa38cc1e794646f389
Matthew H <matej.husak@icloud.com>
2022-05-03 11:34:23 +0200

    created BE endpoints for wallets

--- back/src/app/routes/user.routes.ts
+++ back/src/app/routes/user.routes.ts
@@ -3,6 +3,7 @@ import { MODE_FULL } from "../services/auth/TokenService.js";
 import * as users from "../controllers/user.controller";
 import auth from "../middleware/auth";
 import multer from 'multer';
+import { addOrUpdateWallet } from "../controllers/user.controller";
 
 module.exports = (app: Express) => {
   const upload = multer({ dest: '../uploads/' })
@@ -10,7 +11,7 @@ module.exports = (app: Express) => {
 
   router.put("/createInvestor", auth(MODE_FULL), users.createInvestor);
 
-  //profile
+  // profile
   router.get("/profile", auth(MODE_FULL), users.getProfile);
   router.post("/profile", auth(MODE_FULL), users.createProfile);
   router.put("/profile", auth(MODE_FULL), users.updateProfile);
@@ -18,8 +19,12 @@ module.exports = (app: Express) => {
 
   router.post("/support/create-request", auth(MODE_FULL), users.createSupportRequest);
 
-  //upload image to s3
+  // upload image to s3
   router.put("/profile/image", auth(MODE_FULL), upload.single('image_data'), users.uploadImg);
+
+  // wallets
+  router.get('/wallets', auth(MODE_FULL), users.getUserWallets)
+  router.post('/wallets', auth(MODE_FULL), users.addOrUpdateWallet)
 
   app.use("/api/users", router);
 };
