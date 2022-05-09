3104db4e471b2bdeb2c81be76832e7c85ae84b7b
Matthew H <matej.husak@icloud.com>
2022-05-09 16:37:43 +0200

    used shortenIfAddress helper, added useEffect for addOrUpdate user wallet

--- front/src/components/ConnectButton.tsx
+++ front/src/components/ConnectButton.tsx
@@ -1,16 +1,25 @@
 import Button from '../@ui/Button/Button'
-import { useEthers } from '@usedapp/core'
+import { useEthers, shortenIfAddress } from '@usedapp/core'
 import { ConnectWallet } from '../services/walletService'
+import { addOrUpdateUserWallet } from '../services/userService'
+import { useEffect } from 'react'
 
 export const ConnectButton = () => {
   const { activate, deactivate, account } = useEthers()
+
+  useEffect(() => {
+    if (account) { addOrUpdateUserWallet(account) }
+  }, [account])
+
+  // todo this fragment of code will be in wallet carousel too
+  // todo its duplicated since callback for WalletConnect (maticnetwork does not work)
 
   return (
     <>
       {account
         ? (
           <Button variant='normal' onClick={() => { deactivate() }}>
-            {`${account.slice(0, 6)}...${account.slice(-6)}`}
+            {shortenIfAddress(account)}
           </Button>
           )
         : (
