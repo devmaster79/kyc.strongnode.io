e1386eb58d62ffe0357445845392f08358774995
Matthew H <matej.husak@icloud.com>
2022-05-09 16:26:17 +0200

    walletConnect type

---
+++ front/src/types/wallet-connect.d.ts
@@ -0,0 +1,2 @@
+declare module '@maticnetwork/walletconnect-provider/dist/walletconnect-provider.umd' { // @ts-ignore
+  import WalletConnectProvider from '@maticnetwork/walletconnect-provider/src/index'; export default WalletConnectProvider }
