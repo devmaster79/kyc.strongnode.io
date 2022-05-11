62717dc933b34cdca41c652235ad2667eef8a199
Matthew H <matej.husak@icloud.com>
2022-05-11 11:53:44 +0200

    added coinTypesDictionary, added interfaces

--- front/src/services/walletService.ts
+++ front/src/services/walletService.ts
@@ -1,17 +1,36 @@
-import { Polygon, Config, useTokenBalance } from '@usedapp/core'
+import { Polygon, Config } from '@usedapp/core'
 import WalletConnectProvider from '@maticnetwork/walletconnect-provider/dist/walletconnect-provider.umd'
+
+interface IStringDictionary {
+  [key:string]: string
+}
+
+interface IDictionary {
+  [key:string]: string | boolean
+}
 
 /**
  * Token addresses' dictionary.
  */
-export const tokenAddressDictionary = {
-  strongnode: '0x32934CB16DA43fd661116468c1B225Fc26CF9A8c'
+export const tokenAddressDictionary: IStringDictionary = {
+  strongnode: '0x32934CB16DA43fd661116468c1B225Fc26CF9A8c',
+  'matic-network': '0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0'
+}
+
+/**
+ * Network types dictionary.
+ */
+export const coinTypesDictionary: IDictionary = {
+  strongnode: 'token',
+  ethereum: 'ethereum',
+  'matic-network': 'token',
+  bitcoin: false
 }
 
 /**
  * RPC urls for a different networks.
  */
-export const networksRpcDictionary = {
+export const networksRpcDictionary: IStringDictionary = {
   polygon: 'https://polygon-rpc.com'
 }
 
