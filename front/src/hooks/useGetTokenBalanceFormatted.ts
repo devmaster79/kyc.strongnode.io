5b77aae356085c95dcd5cfe528963432836c19e2
Matthew H <matej.husak@icloud.com>
2022-05-11 12:05:21 +0200

    updated hooks

--- front/src/hooks/useGetTokenBalanceFormatted.ts
+++ front/src/hooks/useGetTokenBalanceFormatted.ts
@@ -1,11 +1,10 @@
 import { useTokenBalance } from '@usedapp/core'
 import { ethers } from 'ethers'
-import { tokenAddressDictionary } from '../services/walletService'
 
 /**
  * Returns the current balance of specified token.
  */
-export const useGetTokenBalanceFormatted = (account: string | null | undefined, tokenAddress: string = tokenAddressDictionary.strongnode, queryParams: object = {}) => {
+export const useGetTokenBalanceFormatted = (account: string | null | undefined, tokenAddress?: string | null | undefined, queryParams: object = {}) => {
   const tokenBalance = useTokenBalance(tokenAddress, account, queryParams)
   return tokenBalance && ethers.utils.formatUnits(tokenBalance, 18)
 }
--- front/src/hooks/useTokenBalanceValidated.ts
+++ front/src/hooks/useTokenBalanceValidated.ts
@@ -1,6 +1,8 @@
 import { useTokenBalance } from '@usedapp/core'
 
-export const useTokenBalanceValidated = (tokenAddress: string, account: string) => {
+export const useTokenBalanceValidated = (tokenAddress: string, account: string | null | undefined) => {
+  if (!account) { return undefined }
+
   // todo validate the token address
   return useTokenBalance(tokenAddress, account)
 }
