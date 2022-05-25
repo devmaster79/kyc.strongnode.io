a70eb1d2cc51658af79dc39fe16306fbeeccce14
nikolinaapetrovic <nikolinaapetrovic97@gmail.com>
2022-05-25 11:54:29 +0200

    Allow numbers in usernames #381 (#382)
    * Allow numbers in usernames #381
    
    * Auth merge update

--- shared/endpoints/common.ts
+++ shared/endpoints/common.ts
@@ -4,7 +4,7 @@ import { z } from 'zod'
 export const userNameRule = z
   .string()
   .min(3, { message: 'Username should be at least 3 characters' })
-  .regex(/^[a-z\-_]+$/g, {
+  .regex(/^[a-z0-9\-_]+$/g, {
     message:
-      'Username should only contain lowercase letters including hypen (-) and underscore (_)'
+      'Username should only contain lowercase letters, numbers, hypen (-) and underscore (_)'
   })
