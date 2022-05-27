dd43037faafd61a50c0c17298f57c354ccbdfba8
Matthew H <matej.husak@icloud.com>
2022-05-27 13:56:50 +0200

    ignore autogenerated js files in front/src/contracts

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -21,7 +21,7 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  ignorePatterns: ['*.md', 'src/assets', '*.css'],
+  ignorePatterns: ['*.md', 'src/assets', '*.css', 'src/contracts'],
   plugins: [
     'react',
     'prettier',
