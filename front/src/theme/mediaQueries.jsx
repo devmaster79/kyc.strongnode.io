97051122cbab55fbf742bc40c8f3f96e74ee5874
alextd79 <alextd.dev@gmail.com>
2022-05-28 21:10:05 +0300

    define a media query for laptop

--- front/src/theme/mediaQueries.jsx
+++ front/src/theme/mediaQueries.jsx
@@ -1,6 +1,7 @@
 const Media = {
   phone: '@media only screen and (max-width: 600px)',
-  tablet: '@media only screen and (max-width: 1200px)',
+  tablet: '@media only screen and (max-width: 900px)',
+  laptop: '@media only screen and (max-width: 1200px)',
   desktop: '@media only screen and (min-width: 1201px)'
 }
 
