6857c5e03783307efb6316f6378ab368940cb892
Matthew H <matej.husak@icloud.com>
2022-06-06 10:34:09 +0200

    revert multiswitch changes

--- front/src/@ui/MultiSwitch/MultiSwitch.tsx
+++ front/src/@ui/MultiSwitch/MultiSwitch.tsx
@@ -85,8 +85,6 @@ const SelectWrapper = styled.div((props) => ({
   border: `1px solid ${props.theme.palette.border.light}`,
   borderRadius: '126px',
   width: 'fit-content',
-  position: 'relative',
-
   ul: {
     display: 'flex',
     listStyleType: 'none',
@@ -103,14 +101,13 @@ const SelectWrapper = styled.div((props) => ({
       color: props.theme.palette.text.secondary,
       cursor: 'pointer',
       backgroundColor: 'none',
-      transition: '500ms background-color',
-      zIndex: 10
+      transition: '500ms background-color'
     }
   },
 
   '.active': {
-    background: 'transparent',
-    backgroundColor: 'transparent',
+    background: 'linear-gradient(90.39deg, #aa1fec 0.24%, #7a3bfe 101.6%)',
+    backgroundColor: '#7a3bfe',
     color: props.theme.palette.button.text
   }
 }))
