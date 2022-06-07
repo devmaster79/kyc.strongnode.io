424328ee555cb43278be7810f303f77a11b77722
Matthew H <matej.husak@icloud.com>
2022-06-07 16:11:59 +0200

    updated run.sh

--- blockchain/run.sh
+++ blockchain/run.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 clear
-echo "🚀 Starting localhost blockchain... Contract will be deployed in 3 seconds."
-( (npx hardhat node) & )
-sleep 3
-echo "🚀 Deploying contract..."
-( (npx hardhat run --network localhost scripts/deploy.js) & )
 
-# for properly ending all of the processes, please close the terminal.
+echo "🚀 Starting localhost blockchain... Contract will be deployed in 5 seconds."
+npx hardhat node &
+sleep 5
+
+echo "🚀 Deploying contract..."
+npx hardhat run --network localhost scripts/deploy.js &
+
+wait
