0016b52f408d6efd2af8becbc98199836f30b45c
Matthew H <matej.husak@icloud.com>
2022-07-05 12:07:18 +0200

    removed refresh methods from cryptocurrency.controller and router

--- back/src/app/controllers/cryptocurrency.controller.ts
+++ back/src/app/controllers/cryptocurrency.controller.ts
@@ -1,72 +1,15 @@
-import {
-  CryptocurrencyDataService,
-  isScope,
-  scopeDays
-} from '../services/cryptocurrency/CryptocurrencyDataService'
-import CoinGeckoApi from 'coingecko-api'
+import { isScope } from '../services/cryptocurrency/CryptocurrencyDataService'
 import {
   StrongnodeCoinData as coinChartData,
-  CoinMetricsData as coinMetricsData,
-  CoinMetricsData
+  CoinMetricsData as coinMetricsData
 } from '../models'
 import {
   GetTokenChartData,
-  GetTokensMetrics,
-  RefreshStrongnodeTokenData
+  GetTokensMetrics
 } from 'shared/endpoints/cryptocurrency'
 import { success, zodValidationError } from 'shared/endpoints/responses'
 import { withResponse } from './utils'
 const { Op } = require('sequelize')
-
-const cryptocurrencyDataService = new CryptocurrencyDataService(
-  new CoinGeckoApi(),
-  CoinMetricsData
-)
-
-/**
- * Method that is used for refreshing data for SNE token from Coingecko.
- * @param req
- * @param res
- * @returns {Promise<void>}
- */
-export const refreshStrongnodeTokenData =
-  withResponse<RefreshStrongnodeTokenData.Response>(async (req) => {
-    const data = RefreshStrongnodeTokenData.schema.parse(req.body)
-    if (!isScope(data.scope))
-      return zodValidationError([
-        {
-          code: 'custom',
-          path: ['scope'],
-          message:
-            'Scope parameter is unsupported. Supported scopes: "days", "weeks", "months" and "years."'
-        }
-      ])
-
-    const chartData = await cryptocurrencyDataService.getTokenChartData(
-      scopeDays[data.scope]
-    )
-    const token = 'strongnode'
-    const checkScopedRecord = await coinChartData.findOne({
-      where: { scope: data.scope, token }
-    })
-
-    const symbol = await cryptocurrencyDataService.getTokenSymbol(token)
-
-    if (!checkScopedRecord)
-      await coinChartData.create({
-        data: chartData,
-        token: token,
-        scope: data.scope,
-        symbol: symbol ? symbol : ''
-      })
-    else
-      await coinChartData.update(
-        { data: chartData, scope: data.scope },
-        { where: { scope: data.scope, token: token } }
-      )
-
-    return success({ message: 'succesfully refreshed' })
-  })
 
 /**
  * Method that is used for getting token data for the client.
